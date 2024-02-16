5d40ef213fd52c15928c92e3be20d02694268c6f
Gustavoliv <gustavo.olivaranchal@gmail.com>
2024-02-16 11:36:19 +0100

    first commit

--- src/main/java/com/tutorial/crudmongoback/CrudMongoBackApplication.java
+++ src/main/java/com/tutorial/crudmongoback/CrudMongoBackApplication.java
@@ -3,10 +3,12 @@ package com.tutorial.crudmongoback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CrudMongoBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudMongoBackApplication.class, args);
 	}
+
 }
